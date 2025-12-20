5cbe4515bd5dee514ee7ad17d7b9ff5ebc69c189
Eval-99 <ev1914jw@gmail.com>
2025-12-20 01:58:29 -0600

    Can now search and select item in database and output to screen the result.

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from functools import partial
 
 from textual.app import App, ComposeResult
 from textual.command import Hit, Hits, Provider
-from textual.widgets import Label
+from textual.containers import Container
+from textual.widgets import Input, Label
 
 con = sqlite3.connect("items.db")
 cur = con.cursor()
@@ -20,11 +21,11 @@ class ItemSearch(Provider):
 
         for name in ITEMS:
             score = matcher.match(name)
-            if score > 0 and not self.app.delete and not self.app.update:  # pyright: ignore[reportAttributeAccessIssue]
+            if score > 0 and not self.app.delete and not self.app.update:  # ty:ignore[unresolved-attribute]
                 yield Hit(
                     score,
                     matcher.highlight(name),
-                    partial(self.app.calc, name),  # pyright: ignore[reportAttributeAccessIssue]
+                    partial(self.app.calc, name),  # ty:ignore[unresolved-attribute]
                     help="",
                 )
 
@@ -37,14 +38,31 @@ class CountCalc(App[None]):
     def compose(self) -> ComposeResult:
         self.delete = False
         self.update = False
-        yield Label("", id="output", classes="box")
+        yield Container(Label("Start. Hello", id="startscreen", classes="start"))
 
     def calc(self, name: str) -> None:
         self.item = cur.execute(
             "SELECT * FROM items WHERE name = ? ", (name,)
         ).fetchone()
-        self.label = self.query_one("#output", Label)
-        self.label.update(f"{self.item[0]}, {self.item[1]}, {self.item[2]}")
+
+        res = round(
+            (float(0.300) / float(self.item[2]) * float(self.item[1])),
+            2,
+        )
+
+        # self.mount(Input("Hi", id="inputbox", classes="itemInput"))
+        #
+        # self.mount(
+        #     Label(
+        #         f"{self.item[0]}, {self.item[1]}, {self.item[2]}\n{res}",
+        #         id="output",
+        #         classes="result",
+        #     )
+        # )
+        # self.mount(Input("Hi", id="inputbox", classes="itemInput"))
+
+        self.label = self.query_one("#startscreen", Label)
+        self.label.update(f"{self.item[0]}, {self.item[1]}, {self.item[2]}\n{res}")
 
 
 if __name__ == "__main__":
