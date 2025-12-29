efe0d5289616552fecaf88cca40a0b6f32c39728
Eval-99 <ev1914jw@gmail.com>
2025-12-29 02:01:39 -0600

    The label and input box are now centered.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from functools import partial
 
 from textual.app import App, ComposeResult
 from textual.command import Hit, Hits, Provider
-from textual.containers import Container
+from textual.containers import Center, Container
 from textual.widgets import Input, Label
 
 con = sqlite3.connect("items.db")
@@ -12,20 +12,20 @@ cur.execute("""
 CREATE TABLE IF NOT EXISTS items('name' TEXT, 'count' INTEGER, 'weight' REAL)
 """)
 
-ITEMS = {row[0] for row in cur.execute("SELECT name FROM items").fetchall()}
+items = {row[0] for row in cur.execute("SELECT name FROM items").fetchall()}
 
 
 class ItemSearch(Provider):
     async def search(self, query: str) -> Hits:
         matcher = self.matcher(query)
 
-        for name in ITEMS:
+        for name in items:
             score = matcher.match(name)
-            if score > 0 and not self.app.delete and not self.app.update:  # ty:ignore[unresolved-attribute]
+            if score > 0 and not self.app.delete and not self.app.update:
                 yield Hit(
                     score,
                     matcher.highlight(name),
-                    partial(self.app.calc, name),  # ty:ignore[unresolved-attribute]
+                    partial(self.app.calc, name),
                     help="",
                 )
 
@@ -33,12 +33,13 @@ class ItemSearch(Provider):
 class CountCalc(App[None]):
     COMMANDS = {ItemSearch}
     CSS_PATH = "style.css"
-    # COMMAND_PALETTE_BINDING = "ctrl+backslash"
 
     def compose(self) -> ComposeResult:
         self.delete = False
         self.update = False
-        yield Container(Label("Start. Hello", id="startscreen", classes="start"))
+        yield Container(
+            Center(Label("Start. Hello", id="startscreen", classes="start"))
+        )
 
     def calc(self, name: str) -> None:
         self.item = cur.execute(
@@ -50,16 +51,9 @@ class CountCalc(App[None]):
             2,
         )
 
-        # self.mount(Input("Hi", id="inputbox", classes="itemInput"))
-        #
-        # self.mount(
-        #     Label(
-        #         f"{self.item[0]}, {self.item[1]}, {self.item[2]}\n{res}",
-        #         id="output",
-        #         classes="result",
-        #     )
-        # )
-        # self.mount(Input("Hi", id="inputbox", classes="itemInput"))
+        # If I select again from the command palette it crashes because of these couple of lines.
+        container = self.query_one(Container)
+        container.mount(Center(Input("Hi", id="inputbox", classes="itemInput")))
 
         self.label = self.query_one("#startscreen", Label)
         self.label.update(f"{self.item[0]}, {self.item[1]}, {self.item[2]}\n{res}")
